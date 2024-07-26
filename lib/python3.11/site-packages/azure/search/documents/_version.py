8b6286baf437445fe86ceca6c24417b91bb0a1e4
teme251 <109575299+teme251@users.noreply.github.com>
2024-07-25 21:45:37 -0400

    Initial commit for PyCharm project

---
+++ lib/python3.11/site-packages/azure/search/documents/_version.py
@@ -0,0 +1,8 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+VERSION = "11.5.0"  # type: str
+
+SDK_MONIKER = "search-documents/{}".format(VERSION)  # type: str
